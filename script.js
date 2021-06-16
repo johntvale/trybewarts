fec2a50442477f9872f838bf42920a4a00f6f509
johntvale <johntvale@gmail.com>
2021-06-16 18:05:30 -0400

    Add Requisito 6 + Correçoes do lint

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-// função requisito 3
-const loginButtonClick = document.querySelector('#login-button');
-loginButtonClick.addEventListener('click', login);
-
 function login() {
-  let userLogin = document.querySelector('#login').value;
-  let userPassword = document.querySelector('#password').value;
+  const userLogin = document.querySelector('#login').value;
+  const userPassword = document.querySelector('#password').value;
   if ((userLogin === 'tryber@teste.com') && (userPassword === '123456')) {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
 }
+
+const loginButtonClick = document.querySelector('#login-button');
+loginButtonClick.addEventListener('click', login);
