7f20d17965e3930d19d80eea94ee1074676c67ad
johntvale <johntvale@gmail.com>
2021-06-19 15:20:06 -0400

    Add Requisito 18

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function login() {
 
 const configButtonLogin = document.querySelector('#login-button');
 configButtonLogin.addEventListener('click', login);
+
+// função habilitar / desabilitar botão Submit
+
+function enableButton() {
+  const agreementButton = document.getElementById('agreement');
+  const submitButton = document.getElementById('submit-btn');
+  if (agreementButton.value === 'disagree') {
+    agreementButton.value = 'agree';
+    submitButton.disabled = false;
+  } else if (agreementButton.value === 'agree') {
+    agreementButton.value = 'disagree';
+    submitButton.disabled = true;
+  }
+}
+
+const configCheckBoxAgreement = document.getElementById('agreement');
+configCheckBoxAgreement.addEventListener('click', enableButton);
