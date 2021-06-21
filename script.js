7e25d7a3905e9572878b9c4fd18997ef2e94d3ec
paulrpn <paulrpn@live.com>
2021-06-21 05:41:12 -0300

    Add Requisito 20

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function login() {
 const configButtonLogin = document.querySelector('#login-button');
 configButtonLogin.addEventListener('click', login);
 
-// função habilitar / desabilitar botão Submit
+// Habilitar/desabilitar botão Submit
 
 function enableButton() {
   const agreementButton = document.getElementById('agreement');
@@ -27,3 +27,19 @@ function enableButton() {
 
 const configCheckBoxAgreement = document.getElementById('agreement');
 configCheckBoxAgreement.addEventListener('click', enableButton);
+
+// Contar caracteres do campo comentário
+// Refer: https://pt.stackoverflow.com/questions/25753/como-fazer-um-contador-de-caracteres-de-uma-textarea
+
+function characterCount(userInput) {
+  const sizeArea = 500;
+  const inputTotal = userInput.length;
+  if (inputTotal <= sizeArea) {
+    const leftoverChars = sizeArea - inputTotal;
+    document.getElementById('counter').innerHTML = leftoverChars;
+  } else {
+    document.getElementById('textarea').value = inputTotal; // .substr(0,sizeArea);
+  }
+}
+
+characterCount();
