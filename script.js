c0b93619db00dd6d11b06be76953438b0c2dba3b
paulrpn <paulrpn@live.com>
2021-06-23 08:25:03 -0300

    Add Requisito 21 + correcoes do lint

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-// Autenticação de Login
+// VARIÁVEIS GLOBAIS
 
+const configButtonLogin = document.querySelector('#login-button');
+const configCheckBoxAgreement = document.getElementById('agreement');
+const sendButtonSubmit = document.getElementById('submit-btn');
+
+const returnName = document.createElement('p');
+const returnEmail = document.createElement('p');
+const returnHouse = document.createElement('p');
+let formFamily = '';
+const returnFamily = document.createElement('p');
+const formContents = [];
+const returnContents = document.createElement('p');
+let formEvaluator = '';
+const returnEvaluator = document.createElement('p');
+const returnComments = document.createElement('p');
+
+const mainForm = document.getElementById('evaluation-form');
+
+// AUTENTICAR O LOGIN
 function login() {
   const userLogin = document.querySelector('#login').value;
   const userPassword = document.querySelector('#password').value;
@@ -9,12 +27,9 @@ function login() {
     alert('Login ou senha inválidos.');
   }
 }
-
-const configButtonLogin = document.querySelector('#login-button');
 configButtonLogin.addEventListener('click', login);
 
-// Habilitar/desabilitar botão Submit
-
+// HABILITAR/DESABILITAR O BOTÃO SUBMIT
 function enableButton() {
   const agreementButton = document.getElementById('agreement');
   const submitButton = document.getElementById('submit-btn');
@@ -26,139 +41,96 @@ function enableButton() {
     submitButton.disabled = true;
   }
 }
-
-const configCheckBoxAgreement = document.getElementById('agreement');
 configCheckBoxAgreement.addEventListener('click', enableButton);
 
-// Confirmação do cadastro ao clicar no botão submit
+// COLETAR INFO DA FAMÍLIA ESCOLHIDA
+function collectDataFamily() {
+  const formFamilyOption = document.getElementsByName('family');
+  for (let index = 0; index < formFamilyOption.length; index += 1) {
+    if (formFamilyOption[index].checked) {
+      formFamily = formFamilyOption[index].value;
+    }
+  }
+  return formFamily;
+}
 
-// // capturar informações dos inputs
-// const collectData1 = () => {
-//   const formName = document.getElementById('input-name').value;
-//   const formLastName = document.getElementById('input-lastname').value;
-//   const formEmail = document.getElementById('input-email').value;
-//   const formHouse = document.getElementById('house').value;
-//   const formFamilyOption = document.getElementsByName('family');
-//   let formFamily = '';
-//   const formContentsOptions = document.getElementsByName('content');
-//   const formContents = [];
-//   const formEvaluatorOption = document.getElementsByName('rate');
-//   let formEvaluator = '';
-//   const formComments = document.getElementById('textarea').value;
-// }
+// COLETAR INFO DOS CONTEÚDOS ESCOLHIDOS
+function collectDataContents() {
+  const formContentsOptions = document.getElementsByName('content');
+  for (let index = 0; index < formContentsOptions.length; index += 1) {
+    if (formContentsOptions[index].checked) {
+      formContents.push(` ${formContentsOptions[index].value}`);
+    }
+  }
+  return formContents;
+}
 
-// capturar informações Family
-// const collectDataFamily = (formFamilyOption, formFamily) => {
-//   for (let index = 0; index < formFamilyOption.length; index += 1) {
-//     if (formFamilyOption[index].checked) {
-//       formFamily = formFamilyOption[index].value;
-//     }
-//   }
-//   return formFamily;
-// }
-// const collectDataContents = (formContentsOptions, formContents) => {
-//   for (let index = 0; index < formContentsOptions.length; index += 1) {
-//     if (formContentsOptions[index].checked) {
-//       formContents.push(` ${formContentsOptions[index].value}`);
-//     }
-//   }
-//   return formContents;
-// }
-// const collectDataEvaluator = (formEvaluatorOption, formEvaluator) => {
-//   for (let index = 0; index < formEvaluatorOption.length; index += 1) {
-//     if (formEvaluatorOption[index].checked) {
-//       formEvaluator = formEvaluatorOption[index].value;
-//     }
-//   }
-//   return formEvaluator;
-// }
+// COLETAR INFO DA NOTA DE AVALIAÇÃO
+function collectDataEvaluator() {
+  const formEvaluatorOption = document.getElementsByName('rate');
+  for (let index = 0; index < formEvaluatorOption.length; index += 1) {
+    if (formEvaluatorOption[index].checked) {
+      formEvaluator = formEvaluatorOption[index].value;
+    }
+  }
+  return formEvaluator;
+}
 
-// function showInformation(sendButtonSubmit) {
+// APAGANDO O FORM PREENCHIDO
+function removeForm() {
+  const formOld1 = document.getElementById('container1-form');
+  formOld1.remove();
+  const formOld2 = document.getElementById('container2-form');
+  formOld2.remove();
+  const formOld3 = document.getElementById('container3-form');
+  formOld3.remove();
+  const formOld4 = document.getElementById('container4-form');
+  formOld4.remove();
+  const formOld5 = document.getElementsByClassName('container5-form')[0];
+  formOld5.remove();
+  const removeLine0 = document.getElementsByClassName('lines')[1];
+  removeLine0.remove();
+  const removeLine1 = document.getElementsByClassName('lines')[0];
+  removeLine1.remove();
+}
 
-//   const formName = document.getElementById('input-name').value;
-//   const formLastName = document.getElementById('input-lastname').value;
-//   const formEmail = document.getElementById('input-email').value;
-//   const formHouse = document.getElementById('house').value;
-//   const formFamilyOption = document.getElementsByName('family');
-//   let formFamily = '';
-//   const formContentsOptions = document.getElementsByName('content');
-//   const formContents = [];
-//   const formEvaluatorOption = document.getElementsByName('rate');
-//   let formEvaluator = '';
-//   const formComments = document.getElementById('textarea').value;
+// COLETAR E ARMAZENAR INFORMAÇÕES DO FORM
+function collectData() {
+  const formName = document.getElementById('input-name').value;
+  const formLastName = document.getElementById('input-lastname').value;
+  const formEmail = document.getElementById('input-email').value;
+  const formHouse = document.getElementById('house').value;
+  collectDataFamily();
+  collectDataContents();
+  collectDataEvaluator();
+  const formComments = document.getElementById('textarea').value;
 
-//   collectDataFamily();
-//   collectDataContents();
-//   collectDataEvaluator();
+  returnName.innerText = `Nome: ${formName} ${formLastName} `;
+  returnEmail.innerText = `Email: ${formEmail}`;
+  returnHouse.innerText = `Casa: ${formHouse}`;
+  returnFamily.innerText = `Família: ${formFamily}`;
+  returnContents.innerText = `Matérias: ${formContents}`;
+  returnEvaluator.innerText = `Avaliação: ${formEvaluator}`;
+  returnComments.innerText = `Observações: ${formComments}`;
+}
 
-//   // identifica opção selecionada do formFamily;
+// CONFIRMAR OS DADOS COLETADOS DO FORM AO CLICAR EM ENVIAR
+function showInformation() {
+  collectData();
+  removeForm();
+  mainForm.appendChild(returnName);
+  mainForm.appendChild(returnEmail);
+  mainForm.appendChild(returnHouse);
+  mainForm.appendChild(returnFamily);
+  mainForm.appendChild(returnContents);
+  mainForm.appendChild(returnEvaluator);
+  mainForm.appendChild(returnComments);
+}
 
-//   // identifica opção selecionada do Conteúdos que quer aprender.
-
-//   // identifica opção selecionada do formFamily;
-
-//   // Remove o conteúdo do Form, e substitui pelas informações geradas (dentro de parágrafos).
-
-//   const newDiv = document.createElement('div');
-
-//   // const variavelMain = document.getElementById('main');
-//   // variavelMain.appendChild(newDiv);
-//   const mainForm = document.getElementById('evaluation-form');
-//   mainForm.appendChild(newDiv);
-
-//   console.log(mainForm.children);
-
-//   const returnName = document.createTextNode(`Nome: ${formName} ${formLastName} `);
-//   newDiv.appendChild(returnName);
-//   const breakLine1 = document.createElement('br');
-//   newDiv.appendChild(breakLine1);
-
-//   const returnEmail = document.createTextNode(`Email: ${formEmail}`);
-//   newDiv.appendChild(returnEmail);
-//   const breakLine2 = document.createElement('br');
-//   newDiv.appendChild(breakLine2);
-
-//   const returnHouse = document.createTextNode(`Casa: ${formHouse}`);
-//   newDiv.appendChild(returnHouse);
-//   const breakLine3 = document.createElement('br');
-//   newDiv.appendChild(breakLine3);
-
-//   const returnFamily = document.createTextNode(`Família: ${formFamily}`);
-//   newDiv.appendChild(returnFamily);
-//   const breakLine4 = document.createElement('br');
-//   newDiv.appendChild(breakLine4);
-
-//   const returnContents = document.createTextNode(`Matérias: ${formContents}`);
-//   newDiv.appendChild(returnContents);
-//   const breakLine5 = document.createElement('br');
-//   newDiv.appendChild(breakLine5);
-
-//   const returnEvaluator = document.createTextNode(`Avaliação: ${formEvaluator}`);
-//   newDiv.appendChild(returnEvaluator);
-//   const breakLine6 = document.createElement('br');
-//   newDiv.appendChild(breakLine6);
-
-//   const returnComments = document.createTextNode(`Observações: ${formComments}`);
-//   newDiv.appendChild(returnComments);
-//   const breakLine7 = document.createElement('br');
-//   newDiv.appendChild(breakLine7);
-
-//   // removendo form antigo
-//   const formOld1 = document.getElementById('container1-form');
-//   formOld1.remove();
-//   const formOld2 = document.getElementById('container2-form');
-//   formOld2.remove();
-//   const formOld3 = document.getElementById('container3-form');
-//   formOld3.remove();
-//   const formOld4 = document.getElementById('container4-form');
-//   formOld4.remove();
-//   const formOld5 = document.getElementsByClassName('container5-form')[0];
-//   formOld5.remove();
-//   const removeLine0 = document.getElementsByClassName('lines')[1];
-//   removeLine0.remove();
-//   const removeLine1 = document.getElementsByClassName('lines')[0];
-//   removeLine1.remove();
-// }
-
-// const sendButtonSubmit = document.getElementById('submit-btn');
-// sendButtonSubmit.addEventListener('click', showInformation);
+// BLOQUEAR O RESET DO FORM QUANDO CLICAR EM ENVIAR
+// Refer: Colaboração da Leticia Turma 13B via dúvida no slack
+function blockFormReset(event) {
+  event.preventDefault();
+  showInformation();
+}
+sendButtonSubmit.addEventListener('click', blockFormReset);
